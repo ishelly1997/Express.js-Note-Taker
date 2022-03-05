55fe94bac2b568ab1658f171f5273eaa7b4311ef
ishelly1997 <ishelly1997@gmail.com>
2022-03-05 16:04:47 -0500

    adding get and post routes

---
+++ db/store.js
@@ -0,0 +1,14 @@
+const uuid = require('uuid/v1');
+const fs = require('fs');
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+class Store{
+    read(){
+        return readFileAsync('/db.json')
+    }
+    write(){}
+    getNotes(){}
+    postNote(){}
+    deleteNote(){}
+}
--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express');
+const res = require('express/lib/response');
 
 const path = require('path');
 
 const notes = require('./db/db.json')
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-/* app.get('/api/notes', (req, res) => {
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
+app.get('/api/notes', (req, res) => {
     let results = notes;
     console.log(req.query)
     res.json(results);
-  }); */
+  });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.post('/api/notes', (req, res) => {
+    res.json(`${req.method} request received to get notes`);
+
+    console.info(`${req.method} request received to get notes`);
+  });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
