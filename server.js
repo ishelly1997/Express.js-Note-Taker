766f07ac33214e4a147910d3c6db7bd1aa571dda
ishelly1997 <ishelly1997@gmail.com>
2022-03-06 22:10:11 -0500

    working through note storage issue

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-//const res = require('express/lib/response');
-const uuid = require('uuid');
+const res = require('express/lib/response');
+const uuid = require('./uuid');
 const path = require('path');
 const notes = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
@@ -9,9 +9,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 const fs = require('fs');
-//const util = require('util');
-//const readFileAsync = util.promisify(fs.readFile);
-//const writeFileAsync = util.promisify(fs.writeFile);
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
@@ -22,9 +22,23 @@ app.use(express.static('public'));
 })*/
 
 app.get('/api/notes', (req, res) => {
-  console.log(req);
-  res.sendFile(path.join(__dirname, './db/db.json'));
+  res.json(`${req.method} get notes`);
+  console.info(`${req.method} get notes`);
 });
+/*app.get('/api/notes/:note_id', (req, res) => {
+  if (req.body && req.params.note_id) {
+    console.info(`${req.method} note request`);
+    const noteId = req.params.note_id;
+    for (let i = 0; i < notes.length; i++) {
+      const activeNote = notes[i];
+      if (activeNote.note_id === noteId) {
+        res.json(activeNote);
+        return;
+      }
+    }
+    res.json('N/A');
+  }
+});*/
 
 app.post('/api/notes', (req, res) => {
   console.log(req);
@@ -42,7 +56,7 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
-  app.get('*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
