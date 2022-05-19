f716b0f3331c9f9ccb1de6338dc81207c37e6efe
ishelly1997 <ishelly1997@gmail.com>
2022-05-19 15:26:25 -0400

    routes

--- server.js
+++ server.js
@@ -10,56 +10,73 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const util = require('util');
-//const readFileAsync = util.promisify(fs.readFile);
-//const writeFileAsync = util.promisify(fs.writeFile);
+
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
-/*app.get('/', (req, res) => {
-  console.log(req)
-  res.sendFile(path.join(__dirname, './public/index.html'))
-})*/
-
 app.get('/api/notes', (req, res) => {
-  res.json(`${req.method} get notes`);
-  console.info(`${req.method} get notes`);
+    res.json(notes.slice(1));
 });
-/*app.get('/api/notes/:note_id', (req, res) => {
-  if (req.body && req.params.note_id) {
-    console.info(`${req.method} note request`);
-    const noteId = req.params.note_id;
-    for (let i = 0; i < notes.length; i++) {
-      const activeNote = notes[i];
-      if (activeNote.note_id === noteId) {
-        res.json(activeNote);
-        return;
-      }
-    }
-    res.json('N/A');
-  }
-});*/
 
-app.post('/api/notes', (req, res) => {
-  console.log(req);
-  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
-  const newNotes = req.body;
-  newNotes.id = uuid();
-  notes.push(newNotes);
-  console.log(notes);
-  fs.writeFileSync('./db/db.json', JSON.stringify(notes))
-  res.json(notes)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-  console.log(req)
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-})
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+});
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
+});
+
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, notes);
+    res.json(true);
+});
 
 app.listen(PORT, () => {
-    console.log(`App server now on port ${PORT}!`);
+    console.log(`API server now on port ${PORT}!`);
 });
--- uuid.js
+++ uuid.js
@@ -1,4 +1,7 @@
+/* const { UUID } = require("sequelize/types");
+
 module.exports = () =>
   Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
     .substring(1);
+module.exports UUID; */
